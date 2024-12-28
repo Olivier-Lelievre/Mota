df96520219e1c42a98e8800d28dc2494ed216944
Olivier-Lelievre <iamkirkkelly@gmail.com>
2024-12-28 15:03:23 +0100

    HomePage avec Hero et galerie

--- front-page.php
+++ front-page.php
@@ -1,3 +1,42 @@
 <?php get_header(); ?>
 
-<?php get_footer(); ?>
+<!-- Hero & image aléatoire -->
+
+<div class="zoneHero">
+    <div class="textHero">
+        <p>PHOTOGRAPHE EVENT</p>
+    </div>
+    <?php echo do_shortcode('[my_random_photo]'); ?>
+</div>
+
+
+<!-- Galerie d'images -->
+
+<div class="image-gallery-wrapper">
+    <div class="image-gallery">
+        <?php 
+        // 1. On définit les arguments pour définir ce que l'on souhaite récupérer
+        $args = array(
+            'post_type' => 'photo',
+            'posts_per_page' => 8,
+        );
+
+
+        // 2. On exécute la WP Query
+        $my_query = new WP_Query( $args );
+
+        // 3. On lance la boucle !
+        if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+            
+            // 4. On importe le fichier photo-block.php contenat ce qui est affiché
+            get_template_part( 'assets/parts/photo-block' );
+
+        endwhile;
+        endif;
+
+        // 4. On réinitialise à la requête principale (important)
+        wp_reset_postdata(); ?>
+    </div>
+</div>
+
+<?php get_footer();
--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ add_theme_support( 'title-tag' );
 
 
 
+// Chargement des styles et scripts
 
 function nmota_register_assets() {
     // Déclarer jQuery
@@ -18,7 +19,6 @@ function nmota_register_assets() {
     wp_enqueue_script('nmota', get_template_directory_uri() . '/assets/js/script.js', ['jquery'], '1.0', true);
 
 // Champ Réf. photo prérempli : localiser les données ACF uniquement si nécessaire
-
     if (is_singular('photo')) {
         // Récupérer l'ID du post
         $post_id = get_the_ID();
@@ -30,9 +30,7 @@ function nmota_register_assets() {
         ]);
     }
 
-    // Déclarer le fichier style.css à la racine du thème
-    wp_enqueue_style('nmota-style', get_stylesheet_uri(), [], '1.0');
-    // Déclarer un fichier CSS supplémentaire
+// Déclarer le fichier style.css à la racine du thème
     wp_enqueue_style('nmota-theme', get_template_directory_uri() . '/assets/css/theme.css', [], '1.0');
 }
 add_action('wp_enqueue_scripts', 'nmota_register_assets');
@@ -59,6 +57,7 @@ add_action( 'after_setup_theme', 'nmota_register_footer_menu' );
 
 
 // création des CPT
+
 // !!!!! Ne pas oublier d'enregistrer les modifications dans le menu Réglages/Permaliens après la création d'un CPT !!!!!
 function nmota_register_post_types() {
     // création du CPT Photo
@@ -82,3 +81,60 @@ function nmota_register_post_types() {
 	register_post_type( 'photo', $args ); // !!!!! SLUG !!!!! Une fois le CPT enregistré, ne jamais changer ce nom 'photo' (slug) !!!!!
 }
 add_action( 'init', 'nmota_register_post_types' );
+
+
+
+
+
+
+// fonction et shortcode pour afficher une photo aléatoire dans le hero de la page d'accueil
+
+function my_random_photo_shortcode() {
+    // 1. Préparer la variable de sortie
+    $output = '';
+
+    // 2. Configurer la WP Query
+    $args = array(
+        'post_type' => 'photo', // CPT "photo"
+        'posts_per_page' => 1, // 1 seul post
+        'orderby' => 'rand', // Aléatoire
+        'tax_query' => array( // Filtrer par la taxonomie "format"
+            array(
+                'taxonomy' => 'format',
+                'field' => 'slug',
+                'terms' => 'paysage', // Terme "paysage"
+            ),
+        ),
+    );
+
+    // 3. Exécuter la requête
+    $query = new WP_Query($args);
+
+    // 4. Vérifier s'il y a des posts
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+
+            // 5. Récupérer l'image via un champ personnalisé ACF
+            $image = get_field('image'); 
+            if ($image) {
+                // Ajouter un conteneur avec l'image en background
+                $output .= '<div class="random-photo-background" style="background-image: url(' . esc_url($image['url']) . ');"></div>';
+
+            }
+        }
+        // Réinitialiser la requête
+        wp_reset_postdata();
+    } else {
+        // Si aucun post n'est trouvé
+        $output .= '<p>Aucune image trouvée.</p>';
+    }
+
+    // 6. Retourner le résultat
+    return $output;
+}
+
+// Ajouter le shortcode
+add_shortcode('my_random_photo', 'my_random_photo_shortcode');
+// Autoriser les shortcodes dans les widgets texte
+add_filter('widget_text', 'do_shortcode');
